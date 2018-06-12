967b9252b1404585f2a6533a69e792420e0dfc77
unknown <XXBCaoFL@HeartyGroup.com>
2018-06-12 14:50:41 +0800

    init

---
+++ js/spider.js
@@ -0,0 +1,13 @@
+var data = [];
+//通过HTTPAnalyzer工具分析请求数据地址
+//请求的数据的地址是变化的，具有有效性，需要在mp.weixin.qq.com域名下进行请求
+//for(var x = 0; x < 30; x++){
+	//$.ajax({
+	//type:'get',
+	//url: 'https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&__biz=MzA5ODM5MDU3MA==&f=json&offset='+ x* 10 +'&count=10&is_ok=1&scene=124&uin=MjUyNjYyNzc2MA%3D%3D&key=b2f834e3138a31c0d043d6d14915eeb7d98f53c1bffbeb1d5a20b9ef2a1d3cffa29389d0ce71b4a6d9e0b4e89a4d244ede7e03a219c9aae997a6d928fd61535a8e0e2da32c8bc9c05e6688a797fcc925&pass_ticket=l0uWVsHd17DFphu27ir%2FwKOVrG%2FvMzMlXw7FSjZ6XdIJbPEI1gO6VmCz5Duy0F3r&wxtoken=&appmsg_token=960_OVJ%252Bqx%252BnoDgKmzsmrnWF3KRuzuP9f6y52LEMaQ~~&x5=0&f=json%20HTTP/1.1',
+	//success: function(result){
+		//data.push(result);
+		//console.log(result);
+	//}
+//})
+//}
